73b38e68dc43696080f7b96ad9e58bad197c9b5d
teukumunawark <munawartmk@gmail.com>
2022-12-24 10:18:04 +0700

    add utils

---
+++ utils/cloudinary.js
@@ -0,0 +1,13 @@
+const cloudinary = require("../../src/config/cloudinary");
+
+class Cloudinary {
+  static async upload(data) {
+    const fileBase64 = data.buffer.toString("base64");
+    const file = `data:${data.mimetype};base64,${fileBase64}`;
+    const cloudinaryImage = await cloudinary.uploader.upload(file);
+
+    return cloudinaryImage;
+  }
+}
+
+module.exports = Cloudinary;
